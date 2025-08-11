cf3be9aaa7e300292fef10372823383671860a45
Samm <105073478+Samm8g@users.noreply.github.com>
2025-08-11 12:00:30 +0300

    Create activity_data.py

---
+++ activity_data.py
@@ -0,0 +1,12 @@
+rich_presence_content = {
+    "state": "I'm a developer!",
+    "details": "Working on: My Cool Project",
+    "activity_type": 0, # 0, 2, 3, 5
+    "large_image": "https://example.com/image.png/",
+    "large_text": "Text to see when hovering",
+    "start": None, # will configured on main.py
+    "buttons": [
+        {"label": "Button 1", "url": "https://example.com/"},
+        {"label": "Button 2", "url": "https://example.com/"}
+    ]
+}
